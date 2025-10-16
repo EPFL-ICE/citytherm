ab6efc834634f79a80faf063e5469f68339d9c22
cy-len <cyprien.lengagne@gmail.com>
2025-10-16 13:52:00 +0200

    fix: code cleanup

---
+++ frontend/src/components/TwoPanesLayout.vue
@@ -0,0 +1,56 @@
+<script setup lang="ts">
+
+const props = defineProps<{
+  title: string;
+}>();
+
+</script>
+
+<template>
+  <div class="scaffold">
+    <v-sheet class="left-bar" elevation="12">
+      <v-card class="d-flex flex-column h-100" flat>
+        <v-card-text class="flex-grow-0">
+          <h2>{{ props.title }}</h2>
+        </v-card-text>
+        <v-card-text class="left-pane-content">
+          <slot name="left-pane"></slot>
+        </v-card-text>
+      </v-card>
+    </v-sheet>
+    <div class="content">
+      <slot></slot>
+    </div>
+  </div>
+</template>
+
+<style>
+.scaffold {
+  --left-bar-width: min(max(300px, 25vw), 400px);
+  display: grid;
+  grid-template-areas: 'left-bar content';
+  grid-template-columns: var(--left-bar-width) calc(100vw - var(--left-bar-width));
+  grid-template-rows: 1fr;
+}
+
+.left-bar {
+  grid-area: left-bar;
+  height: 100dvh;
+  overflow-y: auto;
+  position: relative;
+  z-index: 1;
+}
+
+.content {
+  grid-area: content;
+  height: 100dvh;
+  position: relative;
+}
+
+.left-pane-content {
+  flex: 1;
+  display: grid;
+  grid-template-rows: 1fr auto min-content;
+  overflow-y: auto;
+}
+</style>
