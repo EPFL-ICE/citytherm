7f25d657bbd67eb367c43bac0fbd1f36e4b95731
cy-len <cyprien.lengagne@gmail.com>
2025-11-07 00:28:30 +0100

    fix: grass soil type

--- frontend/src/lib/3d/materials.ts
+++ frontend/src/lib/3d/materials.ts
@@ -118,7 +118,8 @@ const codeToColor: { [key: number]: THREE.Color } = {
   2007: new THREE.Color().setRGB(0.25, 0.25, 0.25), // Asphalt
   2045: new THREE.Color().setRGB(0.6, 0.6, 0.6), // High albedo pavement
   2011: new THREE.Color().setRGB(0.8, 0.7, 0.5), // Sandy loam
-  2018: new THREE.Color().setRGB(0.1, 0.5, 0.9) // Water reservoir
+  2018: new THREE.Color().setRGB(0.1, 0.5, 0.9), // Water reservoir
+  3000: new THREE.Color().setRGB(0.0, 0.7, 0.0) // Grass
 } as const
 
 export function simulationSoilTypeCodeToColor(code: number): THREE.Color {
